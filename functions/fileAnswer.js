e89af5847d970a2426d72f6439344d677dfe431f
Akshay <akshay.kamble@copods.co>
2024-03-09 23:09:36 +0530

    update: working cloud function

---
+++ functions/fileAnswer.js
@@ -0,0 +1,61 @@
+const { openai } = require("./openai.js");
+const { MemoryVectorStore } = require( "langchain/vectorstores/memory")
+const { OpenAIEmbeddings } = require( "@langchain/openai")
+const { CharacterTextSplitter } = require("langchain/text_splitter")
+const { PDFLoader } = require("langchain/document_loaders/fs/pdf")
+
+
+
+
+const createStore = (docs) =>
+  MemoryVectorStore.fromDocuments(docs, new OpenAIEmbeddings());
+
+
+
+const docsFromPDF = () => {
+  const loader = new PDFLoader("./depositor_LOTO.pdf");
+  return loader.loadAndSplit(
+    new CharacterTextSplitter({
+      separator: ". ",
+      chunkSize: 2500,
+      chunkOverlap: 200,
+    })
+  );
+};
+
+const loadStore = async () => {
+  // const videDocs = await docsFromYoutubeVideo(video);
+  const pdfDocs = await docsFromPDF();
+  return createStore([...pdfDocs]);
+};
+
+ const query = async (chatMessage) => {
+  const store = await loadStore();
+  const results = await store.similaritySearch(chatMessage, 2);
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    temperature: 0,
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: `Answer the following question using the provided context. If you cannot answer the question with the context, don't lie and make up stuff. Just say you need more context. 
+        Question: ${chatMessage} 
+        Context: ${results.map((r) => r.pageContent).join("\n")}`,
+      },
+    ],
+  });
+
+  console.log(
+    `Answer: ${response?.choices[0]?.message?.content} \n Sources: ${results
+      ?.map((r) => r?.metadata?.source)
+      .join(", ")}`
+  );
+  return response?.choices[0]?.message?.content
+};
+
+
+exports.query = query;
--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const {query} = require('./fileAnswer')
 admin.initializeApp();
 
 exports.capitalizeMessage = functions.https.onRequest(async (req, res) => {
@@ -8,9 +9,11 @@ exports.capitalizeMessage = functions.https.onRequest(async (req, res) => {
   if (req.method !== 'POST') {
     res.status(405).send('Method Not Allowed');
     return;
-  }
-  const message = req.body.data.text;
-  console.log('message', {message,body:req.body});
+}
+const message = req.body.data.text;
+console.log('message', {message,body:req.body});
+  const AIResponse = await query(message);
+    console.log('AIResponse', AIResponse);
   if (!message) {
     res.status(400).send('No message provided');
     return;
@@ -19,7 +22,7 @@ exports.capitalizeMessage = functions.https.onRequest(async (req, res) => {
   const capitalizedMessage = message.toUpperCase();
   const chatsRef = admin.firestore().collection('chats');
   await chatsRef.add({
-    text: capitalizedMessage,
+    text: AIResponse,
     createdAt: admin.firestore.FieldValue.serverTimestamp(),
     user: {_id: 4, name: 'Automated Capitalizer'}, // Assuming user 4 is your bot
   });
